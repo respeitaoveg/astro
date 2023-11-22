25996d92c3013b7dd4aec79d535035f857cf8238
respectheveg <yuri.braga@codesign.ag>
2023-11-22 15:43:06 -0300

    feat: create step by step, footer and svg

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,17 +6,44 @@ export default {
 			colors: {
 				primary: '#ccff33',
 				secondary: '#000',
-				dark: '#000',
+				dark: '#303030',
 				light: '#000'
-			},
-      backgroundColor: {
-        primary: '#ccff33',
-        dark: '#303030'
-      }
+			}
 		},
 		fontFamily: {
 			'sans': 'system-ui'
-		}
+		},
+    animation: {
+      'bounce-hero': 'hero 3s ease-in-out infinite',
+      'bounce-arrow-right': 'arrow-right 1s ease-in-out infinite',
+      'bounce-arrow-bottom': 'arrow-bottom 1s ease-in-out infinite',
+    },
+    keyframes: {
+      'hero': {
+        '0%, 100%': {
+          transform: 'translateY(-10px)',
+        },
+        '50%': {
+          transform: 'translateY(0)',
+        }
+      },
+      'arrow-right': {
+        '0%, 100%': {
+          transform: 'translateX(0)',
+        },
+        '50%': {
+          transform: 'translateX(5px)',
+        }
+      },
+      'arrow-bottom': {
+        '0%, 100%': {
+          transform: 'translateY(0) rotate(90deg)',
+        },
+        '50%': {
+          transform: 'translateY(5px) rotate(90deg)',
+        }
+      },
+    }
 	},
 	plugins: [],
 }
