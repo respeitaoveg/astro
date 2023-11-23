d787c3883a0f342ae42f27024ca05473d8aca880
respectheveg <yuri.braga@codesign.ag>
2023-11-23 12:57:47 -0300

    build: add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   server: {
     host: true
   },
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: "server",
+  adapter: netlify()
 });
